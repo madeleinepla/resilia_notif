d6c3889534f924b6d6f9cee473b6015184195d02
Madeleine Pla <madeleine.pla7@gmail.com>
2022-09-21 10:48:46 -0400

    Migrate

--- db/migrate/20220921144245_create_notifs.rb
+++ db/migrate/20220921144245_create_notifs.rb
@@ -1,7 +1,11 @@
 class CreateNotifs < ActiveRecord::Migration[5.2]
   def change
     create_table :notifs do |t|
-
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :seen, null: false
+      
+      
       t.timestamps
     end
   end
